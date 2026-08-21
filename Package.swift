8fb5c19247b65ac89f5ece5601ddc210d23dd023
Jarrod Norwell <official.antique@gmail.com>
2026-08-22 01:07:14 +0800

    Change iOS platform version from 17 to 16

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AntiqueKit",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v16)
     ],
     products: [
         .library(name: "AntiqueKit", targets: ["AntiqueKit"]),
